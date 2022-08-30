92d3af7dcb9f38f707c1bf278bc20dc9618c3a8e
natnael-OQP <makiya112545@gmail.com>
2022-08-30 10:20:23 +0300

    random data

---
+++ lib/model/data.dart
@@ -0,0 +1,27 @@
+class Result {
+  final String result, type;
+
+  Result({
+    required this.result,
+    required this.type,
+  });
+}
+
+List<Result> demo_results = [
+  Result(
+    type: "LDL",
+    result: "120mg/dL",
+  ),
+  Result(
+    type: "HDL",
+    result: "120mg/dL",
+  ),
+  Result(
+    type: "LDL",
+    result: "120mg/dL",
+  ),
+  Result(
+    type: "LDL",
+    result: "120mg/dL",
+  ),
+];
